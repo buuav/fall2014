1feb90aadb75a0fca1069d549687ac2613f3281f
ahuang20 <ahuang20@bu.edu>
2014-11-07 13:25:21 -0500

    Pin Fixes
    Added in values for some pins

--- hover/hover.ino
+++ hover/hover.ino
@@ -1,10 +1,16 @@
-const int rcPin1 = 0;//?
-const int rcPin2 = 0;//?
-const int rcPin3 = 0;//?
-const int rcPin4 = 0;//?
-const int rcPin5 = 0;//?
+const int rcPin1 = A0;
+const int rcPin2 = A1;
+const int rcPin3 = A2;
+const int rcPin4 = A3;
+const int rcPin5 = A4;
 
-const int xBeePin = 0;//?
+const int xBeePin = 0;
+
+const int multiWiiPin1 = 5;
+const int multiWiiPin2 = 6;
+const int multiWiiPin3 = 9;
+const int multiWiiPin4 = 10;
+const int multiWiiPin5 = 11;
 
 const int GROUNDED = 0;
 const int MANUAL = 1;
@@ -18,12 +24,24 @@ const long AUX_THRESHOLD = 2000;//?
 int _mode;
 int _oldMode;
 
+struct Inputs{
+  long rcPin1,
+  long rcPin2,
+  long rcPin3,
+  long rcPin4,
+  long rcPin5
+};
+
+Inputs _inputs;
+
 struct State{
   float Z;
   float Yaw;
   float Pitch;
   float Roll;
-}
+};
+
+State _state;
 
 void setup()
 {
@@ -105,7 +123,9 @@ void updateState(){
   
 }
 
-
+State getState(){
+  return _state;
+}
 
 void groundedAct(){
   
@@ -119,4 +139,4 @@ void autoAct(){
   
 }
 
-
+void updateInputs()
